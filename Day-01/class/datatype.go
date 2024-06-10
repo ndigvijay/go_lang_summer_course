0c5c6728ab944b7bf7db0226adc7620cf66bfbc6
Digvijay Narayan <digvijaynarayan@Digvijays-Laptop.local>
2024-06-10 10:20:58 +0530

    intial commit

---
+++ Day-01/class/datatype.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("datatypes")
+	// int
+		// uint
+		// int8,int16,int32,int64-signed integer
+		// uint8,uint16,uint32,uint64-unsigned integer
+
+	// float
+		// float32 float64
+
+	// String
+		// string
+	
+	// Boolean
+		// bool-true,false 
+
+	// Arrays
+		// array-same datatypes, cannot increase size
+	// slice
+		//dynamically sized data structure
+	// map
+		//key value pairs
+	// struct
+		// user defined datatypes
+	// interface
+
+
+
+
+
+
+}
---
+++ Day-01/class/prg1.go
@@ -0,0 +1,16 @@
+//comments yo
+
+// structure of go
+// package
+// import statement
+// custom functions
+// statement and expression
+
+package main
+import "fmt";
+
+
+func main(){
+	fmt.Println("hello world")
+
+}
